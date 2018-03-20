14c8ae5560d7adb80af85b2e84248fd6fc5be29c
mirelatv <noelia.mirela@gmail.com>
2018-03-20 18:02:00 -0500

    subiendo hora

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,23 +1,19 @@
 import React from 'react'
 
+var d = new Date();
 
+console.log(d.getHours());
 const Clock = (date) => (
 
 
   <div className=' col-4 col-md-4 container_clock rounded-right'>
     <img className='clock float-left' src="https://image.flaticon.com/icons/svg/19/19851.svg" alt="" />
     <p className='text-center'>Tu orden expirará </p>
+    <p><span>{d.getHours()}</span>
+    <span> :{d.getMinutes()}</span>
+    
+    </p>
   </div>
 )
-
-
-
-//   <div>
-//     <p> Su pedido expira</p>
-//     <p>  {date} </p>
-//   </div>
-//   )
-
-//   setInterval(date, 1000);
 
 export default Clock
