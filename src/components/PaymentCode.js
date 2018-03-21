c444f5fe0c956f8d4d9e33934b767ef867296dde
andrea-isabel <andrea19.93.15@gmail.com>
2018-03-20 19:08:36 -0500

    estilos

--- src/components/PaymentCode.js
+++ src/components/PaymentCode.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const PaymentCode = () => (
-  <div className="subcontainer_input_PaymentCode col-4 col-md-4 py-2">
+  <div className="subcontainer_input_PaymentCode col-3 py-2">
     <p>Código de pago</p>
     <form>
       <div className="row">
         <div className="col">
-        <input className="form-control" type="text" />
+        <input className="form-control" type="text" maxLength="8" />
         </div>
       </div>
     </form>
