714a16cf66e14b7ac42d9190d7c73390e37073f2
mirelatv <noelia.mirela@gmail.com>
2018-03-20 19:10:04 -0500

    subiendo col de  footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
       <li className=" print nav-item col-md-3 offset-md-1">Instrucciones vía
         <a className='containerPrint ' href=""><img src="../assets/icon-print.svg" alt="Print"   width="25px"/></a>
       </li>
-      <li className=" mail nav-item col-md-4">Ayuda:  <a className=''href="mailto:contacto@redbus.pe">contacto@redbus.pe</a>   </li>
+      <li className=" mail nav-item col-md-3 offset-md-1">Ayuda:  <a className=''href="mailto:contacto@redbus.pe">contacto@redbus.pe</a>   </li>
       <li className=" logoFooter nav-item col-md-3 offset-md-1">Pago via
         <img className="m-3" src="assets/logo-pagoefectivo.svg" alt="Redbus - PagoEfectivo" width="86px" />
       </li>
