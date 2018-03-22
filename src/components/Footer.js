7a2b19026aa3354717354aad9631b18af9b596d7
mirelatv <noelia.mirela@gmail.com>
2018-03-22 00:15:21 -0500

    subiendo footer  con componente PrintComponenet

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import PrintThisComponent from './PrintThisComponent'
 
 const Footer = () => (
   <footer className='container'>
-    <ul className='nav row d-flex align-item-center'>
+    <ul className='nav row d-flex align-items-center justify-content-center'>
     <PrintThisComponent/>
-      <li className=" mail nav-item col-md-5 col-7">Ayuda:  <a className=''href="mailto:contacto@redbus.pe">contacto@redbus.pe</a>   </li>
-      <li className=" logoFooter nav-item col-md-4  col-5">Pago via
+      <li className="text-center mail nav-item col-md-4 col-6">Ayuda:  <a className=''href="mailto:contacto@redbus.pe">contacto@redbus.pe</a>   </li>
+      <li className=" text-center logoFooter nav-item col-md-4  col-5">Pago via
         <img className="m-3" src="assets/logo-pagoefectivo.svg" alt="Redbus - PagoEfectivo" width="86px" />
       </li>
     </ul>
--- src/components/PrintThisComponent.js
+++ src/components/PrintThisComponent.js
@@ -1,6 +1,6 @@
 
- 
-import React, {Component} from 'react'
+
+import React, { Component } from 'react'
 
 
 class PrintThisComponent extends Component {
@@ -14,16 +14,11 @@ class PrintThisComponent extends Component {
   render() {
 
     return (
-      <div>
-        <li className=" print nav-item col-md-3 col-12 aling-center">Instrucciones vía
-        <a onClick={() => window.print()} className='containerPrint ' href=""><img src="../assets/icon-print.svg" alt="Print"   width="25px"/></a>
-      </li>
-
-       
-
-       
-
-      </div>
+      
+        <li className="text-center print nav-item col-md-4 col-12 aling-center">Instrucciones vía
+        <a onClick={() => window.print()} className='containerPrint ' href=""><img src="../assets/icon-print.svg" alt="Print" width="25px" /></a>
+        </li>
+     
 
     )
   }
