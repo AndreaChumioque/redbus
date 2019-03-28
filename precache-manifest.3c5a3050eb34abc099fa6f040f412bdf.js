2b6c81ce708af3eec96c10a706ede17230c67b62
andreachumioque <aichumioque@gmail.com>
2019-03-28 12:36:14 -0500

    Updates

---
+++ precache-manifest.3c5a3050eb34abc099fa6f040f412bdf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/redbus/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/redbus/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/redbus/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/redbus/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "48b31384c78efb93d175",
+    "url": "/redbus/static/js/runtime~main.f18ccf77.js"
+  },
+  {
+    "revision": "93b11af7c73e3680602c",
+    "url": "/redbus/static/js/main.aa1c54e6.chunk.js"
+  },
+  {
+    "revision": "78ebab9d00984661a079",
+    "url": "/redbus/static/js/2.b0e160a1.chunk.js"
+  },
+  {
+    "revision": "78ebab9d00984661a079",
+    "url": "/redbus/static/css/2.346a6855.chunk.css"
+  },
+  {
+    "revision": "2b6d91adef0cef66a6297ac944b19c60",
+    "url": "/redbus/index.html"
+  }
+];
