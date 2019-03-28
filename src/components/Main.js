29693d6f9455167baab4493d358861ad0f789c20
andreachumioque <aichumioque@gmail.com>
2019-03-28 10:55:39 -0500

    update project

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,14 +5,6 @@ import ContentContainer from '../containers/ContentContainer'
 const Main = () => {
   return (
     <section className="container px-0">
-      {/* <ul className="nav nav-tabs px-3">
-        <li className="nav-item">
-          <a className="nav-link py-1" data-toggle="tab" role="tab" >Banca por Internet</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link py-1" data-toggle="tab" role="tab">Pagar en efectivo</a>
-        </li>
-      </ul> */}
       <TabContainer />
       <div className="tab-content p-3">
         <ContentContainer />
